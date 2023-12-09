e0db3c5c3b03152f33505299bc75f351e6a540c2
vikasprajapati-01 <vikasprajapati9980@gmail.com>
2023-12-09 21:36:23 +0530

    Day 5

---
+++ Day5/BubbleSort.java
@@ -0,0 +1,30 @@
+
+import java.util.*;
+
+public class BubbleSort
+
+{
+    // Using 2 for loops to arrange the array
+    static void sort(int[] arr)
+    {
+        for(int i=0; i<arr.length-1; i++)
+        {
+            for(int j=0; j<arr.length-i-1; j++)
+            {
+                if(arr[j] > arr[j+1])
+                {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        int[] arr = {23,34,21,3,12,56,4,33,65,88};
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
